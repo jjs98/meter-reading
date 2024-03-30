fba34133f56ada3cf7b53d9965dc04fcb1f37894
Jens Schmitz <jensschmitz1@hotmail.com>
2024-03-31 00:56:23 +0100

    chore: remove jest

--- jest.config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  transform: {
-    '^.+\\.ts$': ['ts-jest', { tsconfig: 'tsconfig.spec.json' }],
-  },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: 'dist/coverage'
-};
