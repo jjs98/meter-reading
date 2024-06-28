751c2a72b86790b08aac7c2e2b191480df9f65ee
jjs98 <jensschmitz1@hotmail.com>
2024-06-28 19:53:17 +0200

    reset repo

--- transloco.config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  rootTranslationsPath: 'assets/i18n/',
-  availableLangs: ['en', 'de'],
-  fullLangs: ['en-US', 'de-DE'],
-  defaultLang: 'en',
-  defaultFullLang: 'en-US',
-  reRenderOnLangChange: true,
-  keysManager: {},
-};
