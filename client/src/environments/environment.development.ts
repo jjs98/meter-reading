278ef8fd9930a625071be8cfd7b4bc97889f695c
jjs98 <jensschmitz1@hotmail.com>
2024-07-11 21:22:06 +0200

    ✨ add pipeline

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   api: {
     // baseUrl: 'http://localhost:5401',
-    baseUrl: 'https://PC-JJS:5001',
+    baseUrl: 'https://localhost:5001',
   },
 };
