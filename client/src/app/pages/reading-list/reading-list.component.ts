7c94a6013841e37aa9e6942b6af401a9c2f8dcc6
jjs98 <jensschmitz1@hotmail.com>
2024-07-25 15:19:21 +0200

    ✨ add excel export

--- client/src/app/pages/reading-list/reading-list.component.ts
+++ client/src/app/pages/reading-list/reading-list.component.ts
@@ -11,6 +11,7 @@ import {
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import * as FileSaver from 'file-saver';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { ChartModule } from 'primeng/chart';
@@ -57,8 +58,8 @@ export class ReadingListComponent implements OnInit {
   private readonly newDialog = viewChild.required(ReadingDialogComponent);
   private readonly editDialog = viewChild.required(ReadingDialogComponent);
 
-  protected valuesCount = signal(10);
-  protected readonly valuesCountOptions = [5, 10, 15, 20];
+  protected valuesCount = signal('12');
+  protected readonly valuesCountOptions = ['6', '12', '24', 'max'];
   protected meterId = signal(-1);
   protected chartData = computed(() => {
     const labels: string[] = [];
@@ -77,16 +78,16 @@ export class ReadingListComponent implements OnInit {
 
     return {
       labels:
-        this.valuesCount() >= labels.length
+        this.valuesCount() === 'max' || Number(this.valuesCount()) >= labels.length
           ? labels.reverse()
-          : labels.reverse().slice(labels.length - this.valuesCount(), labels.length),
+          : labels.reverse().slice(labels.length - Number(this.valuesCount()), labels.length),
       datasets: [
         {
           label: 'Readings',
           data:
-            this.valuesCount() >= deltaData.length
+            this.valuesCount() === 'max' || Number(this.valuesCount()) >= deltaData.length
               ? deltaData
-              : deltaData.slice(deltaData.length - this.valuesCount(), deltaData.length),
+              : deltaData.slice(deltaData.length - Number(this.valuesCount()), deltaData.length),
           fill: false,
           borderColor: getComputedStyle(document.documentElement).getPropertyValue(
             '--primary-color'
@@ -103,7 +104,7 @@ export class ReadingListComponent implements OnInit {
     },
     scales: {
       y: {
-        beginAtZero: true,
+        beginAtZero: false,
       },
     },
   };
@@ -125,5 +126,51 @@ export class ReadingListComponent implements OnInit {
 
   protected navigateBack(): void {
     this.navigationService.navigateBack();
+  }
+
+  protected exportExcel(): void {
+    import('xlsx').then(xlsx => {
+      const readings = this.dataStore.readings().map(reading => {
+        return {
+          Datum: new Date(reading.readingDate).toLocaleDateString(),
+          Zählerstand: reading.number,
+        };
+      });
+      const worksheet = xlsx.utils.json_to_sheet(readings);
+      const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+      let meter = this.dataStore.meters().find(meter => meter.id === this.meterId());
+      if (!meter) {
+        meter = this.dataStore.sharedMeters().find(meter => meter.id === this.meterId());
+      }
+      let fileName = meter?.location ?? 'Zähler';
+      fileName += meter?.addition ? `_${meter?.addition}` : '';
+      if (meter?.type !== undefined) {
+        switch (meter.type) {
+          case 0:
+            fileName += '_Strom';
+            break;
+          case 1:
+            fileName += '_Wasser';
+            break;
+          case 2:
+            fileName += '_Gas';
+            break;
+        }
+      }
+      this.saveAsExcelFile(excelBuffer, fileName);
+    });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private saveAsExcelFile(buffer: any, fileName: string): void {
+    const EXCEL_TYPE =
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+    const EXCEL_EXTENSION = '.xlsx';
+    const data: Blob = new Blob([buffer], {
+      type: EXCEL_TYPE,
+    });
+    FileSaver.saveAs(data, `${fileName}_${new Date().toLocaleDateString()}${EXCEL_EXTENSION}`);
   }
 }
