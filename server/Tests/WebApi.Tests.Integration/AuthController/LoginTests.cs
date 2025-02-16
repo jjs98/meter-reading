725fee3b3d4dedc440de80986a7e6bc5fcfc59fa
jjs98 <jensschmitz1@hotmail.com>
2025-02-16 02:59:31 +0100

    test: add LoginTests (♡ω♡ ) ~♪

---
+++ server/Tests/WebApi.Tests.Integration/AuthController/LoginTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using Application.DTOs;
+using FluentAssertions;
+
+namespace WebApi.Tests.Integration.AuthController;
+
+public class LoginTests(WebApiFactory webApiFactory) : IClassFixture<WebApiFactory>
+{
+    private readonly HttpClient _client = webApiFactory.CreateClient();
+
+    [Fact]
+    public async Task Login_ReturnsToken_WhenUserExist()
+    {
+        // Arrange
+        var user = webApiFactory.GetTestUser();
+        await webApiFactory.CreateTestUserAsync(user);
+        var login = new UserLoginDto { Username = user.Username, Password = user.Password };
+
+        // Act
+        var response = await _client.PostAsJsonAsync("api/auth/login", login);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        var token = await response.Content.ReadFromJsonAsync<TokenDto>();
+        token.Should().NotBeNull();
+        token.Token.Should().NotBeNullOrEmpty();
+    }
+
+    [Fact]
+    public async Task Login_ReturnsUnauthorized_WhenWrongPasswordIsUsed()
+    {
+        // Arrange
+        var user = webApiFactory.GetTestUser();
+        await webApiFactory.CreateTestUserAsync(user);
+        var login = new UserLoginDto { Username = user.Username, Password = "wrongpassword" };
+
+        // Act
+        var response = await _client.PostAsJsonAsync("api/auth/login", login);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task Login_ReturnsUnauthorized_WhenUserDoesNotExist()
+    {
+        // Arrange
+        var login = new UserLoginDto { Username = "nonexistent", Password = "nonexistent" };
+
+        // Act
+        var response = await _client.PostAsJsonAsync("api/auth/login", login);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+    }
+}
--- server/Tests/WebApi.Tests.Integration/WebApiFactory.cs
+++ server/Tests/WebApi.Tests.Integration/WebApiFactory.cs
@@ -1,3 +1,4 @@
+using Bogus;
 using Infrastructure;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
@@ -6,20 +7,30 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
+using Npgsql;
 using Testcontainers.PostgreSql;
 
 namespace WebApi.Tests.Integration;
 
 public class WebApiFactory : WebApplicationFactory<IApiMarker>, IAsyncLifetime
 {
+    private const string _hashedPassword =
+        "$2a$11$kwUkCA36Ea.qMqLiMLA5xer0FqbTZ0w5.3JalL0jZzKmsw3QF7yM.";
+
+    public List<TestUser> TestUsers { get; } = [];
+
     private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
-        .WithPortBinding(5555)
+        .WithPortBinding(5432, true)
         .Build();
+
+    private readonly Faker<TestUser> _userGenerator = new Faker<TestUser>()
+        .RuleFor(u => u.Username, f => f.Person.UserName)
+        .RuleFor(u => u.Password, f => "password")
+        .RuleFor(u => u.HashedPassword, _hashedPassword)
+        .RuleFor(u => u.Role, "User");
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
-        Console.WriteLine($"ConnectionString: {_dbContainer.GetConnectionString()}");
-
         builder.ConfigureLogging(logging =>
         {
             logging.ClearProviders();
@@ -44,5 +55,57 @@ public class WebApiFactory : WebApplicationFactory<IApiMarker>, IAsyncLifetime
     public new async Task DisposeAsync()
     {
         await _dbContainer.DisposeAsync();
+    }
+
+    public static TestUser GetAdminUser() =>
+        new()
+        {
+            Username = "admin",
+            Password = "password",
+            HashedPassword = _hashedPassword,
+            Role = "Admin"
+        };
+
+    public TestUser GetTestUser()
+    {
+        var user = _userGenerator.Generate();
+        TestUsers.Add(user);
+        return user;
+    }
+
+    public async Task CreateTestUserAsync(TestUser user)
+    {
+        await using var dataSource = NpgsqlDataSource.Create(_dbContainer.GetConnectionString());
+        await using var command = dataSource.CreateCommand();
+        command.CommandText = $"""
+            INSERT INTO "Users" ("Username", "Password", "CreateDate")
+            SELECT '{user.Username}', '{user.HashedPassword}', NOW()
+            WHERE NOT EXISTS (SELECT 1 FROM "Users" WHERE "Username" = '{user.Username}');
+
+            INSERT INTO "Roles" ("Name", "CreateDate")
+            SELECT '{user.Role}', NOW()
+            WHERE NOT EXISTS (SELECT 1 FROM "Roles" WHERE "Name" = '{user.Role}');
+
+            DO $$
+            DECLARE
+                RoleId integer = "Id" FROM "Roles" WHERE "Name" = '{user.Role}';
+                UserId integer = "Id" FROM "Users" WHERE "Username" = '{user.Username}';
+
+            BEGIN
+                INSERT INTO "UserRoles" ("RoleId", "UserId", "CreateDate")
+                SELECT RoleId, UserId, NOW()
+                WHERE NOT EXISTS (SELECT 1 FROM "UserRoles" WHERE "RoleId" = RoleId AND "UserId" = UserId);
+            END $$;
+            """;
+
+        await command.ExecuteNonQueryAsync();
+    }
+
+    public class TestUser
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+        public required string HashedPassword { get; set; }
+        public required string Role { get; set; }
     }
 }
